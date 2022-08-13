d0e4342d7a2caa9975b18398abe7768f02d8500e
PP-2022 <2733286080@qq.com>
2022-08-13 15:29:20 +0800

    up test

--- ChatRoom/src/client/test.java
+++ ChatRoom/src/client/test.java
@@ -44,6 +44,10 @@ public class test {
     }*/
     public static void main(String[] args) {
         System.out.println("Holle World");
+        System.out.println("Holle World");
+        System.out.println("Holle World");
+        System.out.println("Holle World");
+        System.out.println("Holle World");
     }
 
 }
